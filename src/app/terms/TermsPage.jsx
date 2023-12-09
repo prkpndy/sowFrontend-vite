742007bf1332cb681be45b74ff53d646d1de9e5f
Prakhar Pandey <prkpndy@iitk.ac.in>
2023-12-09 22:07:25 +0530

    Remove use client from 	- PriceListPage.jsx 	- HomePage.jsx 	- TermsPage.jsx 	- UsPage.jsx 	- LanguageDropdown.jsx 	- Navbar.jsx

--- src/app/terms/TermsPage.jsx
+++ src/app/terms/TermsPage.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import xss from "xss";
 import { useEffect, useState } from "react";
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { useState, useCallback, useEffect, useRef } from "react";
 import { BiMenu } from "react-icons/bi";
