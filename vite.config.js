138636e21b16c012cf9337b87ce03e248ddd8d35
Prakhar Pandey <prkpndy@iitk.ac.in>
2023-12-09 22:02:06 +0530

    Update vite config to ignore module_leve_directory error type

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,15 @@
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
+});
