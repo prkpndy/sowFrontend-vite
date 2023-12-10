48cb7641b68c57d75d475e3851d958faf4cd4111
Prakhar Pandey <prkpndy@iitk.ac.in>
2023-12-10 22:45:29 +0530

    Update code to solve routing issue - 2

--- src/app/home/HomePage.jsx
+++ src/app/home/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 export default function HomePage() {
   return (
@@ -7,13 +8,16 @@ export default function HomePage() {
       <h3 className="text-xl p-5">Please go to the following links:</h3>
       <ul className="p-5">
         <li>
-          <a href={"/terms"}>/terms</a>
+          {/*<a href={"/terms"}>/terms</a>*/}
+            <Link to={"/terms"}>/terms</Link>
         </li>
         <li>
-          <a href={"/us"}>/us</a>
+          {/*<a href={"/us"}>/us</a>*/}
+            <Link to={"/us"}>/us</Link>
         </li>
         <li>
-          <a href={"/pricelist"}>/pricelist</a>
+          {/*<a href={"/pricelist"}>/pricelist</a>*/}
+            <Link to={"/pricelist"}>/pricelist</Link>
         </li>
       </ul>
     </div>
